a9708d5ea86bec23d4f8e38a8a22a89e157441a8
ahmadalshawakri <ashawakri@gmail.com>
2023-07-10 22:59:04 +0300

    General Button Comp.

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = (props) => {
-  return <button type="button">{props.value}</button>;
+  return <button type="button" className={props.className}>{props.value}</button>;
 };
 
 export default Button;
